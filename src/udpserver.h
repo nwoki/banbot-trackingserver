0e44ff6fb985614cd297c8667f5a4243770ad733
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2011-03-20 15:40:43 +0100

    Added license to files

--- src/udpserver.h
+++ src/udpserver.h
@@ -1,3 +1,25 @@
+/*
+ *      udpserver.h is part of BanBot-TrackingServer
+ *
+ *      BanBot-TrackingServer is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      BanBot-TrackingServer is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with BanBot-TrackingServer (look at GPL_License.txt).
+ *      If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Copyright © 2011 Francesco Nwokeka( n3m3s1s )
+ *      Author           Francesco Nwokeka
+ */
+
+
 #ifndef UDPSERVER_H
 #define UDPSERVER_H
 
